45c8133650059df3e409f667e4b63fa498eef4e8
bcantepe <gamibaris@gmail.com>
2023-04-04 04:05:01 +0300

    ethers.js added

--- getFloorPrice.js
+++ getFloorPrice.js
@@ -1,4 +1,19 @@
 const axios = require("axios");
+const ethers = require("ethers");
+
+require("dotenv").config();
+
+
+const provider = new ethers.providers.JsonRpcProvider(process.env.INFURA_URL);
+const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
+
+
+const contractAddress = "0x7bd29408f11d2bfc23c34f18275bbf23bb716bc7";
+const abi = [];
+
+const contract = new ethers.Contract(contractAddress, abi, provider);
+
+
 
 const collectionSlug = "boredapeyachtclub";
 
